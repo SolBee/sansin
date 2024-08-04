2e16f048df68e5aa0a1b60627946c865dccb9ed5
Sol-Bee <sorub3535@gmail.com>
2024-08-04 14:56:31 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sansin/precache-manifest.08fbec3d93df24179b0563c6e9db488a.js"
+  "/sansin/precache-manifest.191612ac7343f1d807ad3bca6bcc7e8f.js"
 );
 
 self.addEventListener('message', (event) => {
