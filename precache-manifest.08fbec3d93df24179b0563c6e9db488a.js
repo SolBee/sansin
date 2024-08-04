01888d2bf4f86a3a347e43ed79af5221bd8634ec
Sol-Bee <sorub3535@gmail.com>
2024-08-04 14:53:59 +0900

    Updates

---
+++ precache-manifest.08fbec3d93df24179b0563c6e9db488a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bdbf139508ae1eb34e0b00fcd3586d",
+    "url": "/sansin/index.html"
+  },
+  {
+    "revision": "3c7d9160e844ee2a8b60",
+    "url": "/sansin/static/css/2.6cc3709f.chunk.css"
+  },
+  {
+    "revision": "a4d7aff7bf0033a8e571",
+    "url": "/sansin/static/css/main.3530da40.chunk.css"
+  },
+  {
+    "revision": "3c7d9160e844ee2a8b60",
+    "url": "/sansin/static/js/2.069ed3ca.chunk.js"
+  },
+  {
+    "revision": "5819fd5d60a3edb648491d81a7fb4bba",
+    "url": "/sansin/static/js/2.069ed3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598ac04def8bff6214e9",
+    "url": "/sansin/static/js/3.e94ba233.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/sansin/static/js/3.e94ba233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b544c802645794a4f3cd",
+    "url": "/sansin/static/js/4.475d366d.chunk.js"
+  },
+  {
+    "revision": "277781dec9eb97c24cb2ec4f9d920c83",
+    "url": "/sansin/static/js/4.475d366d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7018f9c34f3fe6fb9b",
+    "url": "/sansin/static/js/5.3b9f0b65.chunk.js"
+  },
+  {
+    "revision": "226e5c19780409eea7198a4f18013bfd",
+    "url": "/sansin/static/js/5.3b9f0b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d7aff7bf0033a8e571",
+    "url": "/sansin/static/js/main.aaa63dad.chunk.js"
+  },
+  {
+    "revision": "45768770574cc802adfc",
+    "url": "/sansin/static/js/runtime-main.264195af.js"
+  }
+]);
