88f251ef358650f6c14b718b268616766a80a521
Sol-Bee <sorub3535@gmail.com>
2024-08-06 17:29:12 +0900

    Update app.py to bind to 0.0.0.0

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
